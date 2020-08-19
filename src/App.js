d4408693eec1ce7ca1e63d2ca7ba600745b7335c
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-08-19 21:13:30 +0500

    add file ThankstoGod

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Infopanel from './Components/Infopanel';
 import SimpleAlerts from './Components/Alert';
 import { Footer } from './Components/Footer';
 import './App.css';
+import Nocases from './Components/ThankstoGod';
+
 function App() {
   const screenconfig = useState(0);
   return (
     <div >
       <Header />
       <SimpleAlerts />
+      <Nocases/>
       <Infopanel currentscreen={screenconfig[0]} />
       <Bottomtabs screenconfig={screenconfig} />
-    <div><Footer /></div>
+    <div>
+      <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Components/ThankstoGod.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Nocases(){
+    return(
+        <div className="nocases">
+            <h2 id="nocases h1">
+               No New Cases!  Thanks to Almighty Allah 
+            </h2>
+        </div>
+    )
+};
+export default Nocases;
